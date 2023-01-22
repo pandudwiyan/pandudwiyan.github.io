0ef513efdef751674290c92fdabaa91c36bc6476
unknown <pandudwiyan@gmail.com>
2023-01-22 21:48:06 +0700

    first commit

---
+++ _next/static/chunks/78e521c3-546479c5588b5e37.js
@@ -0,0 +1,49 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [937],
+    {
+        9352: function (t, a, n) {
+            n.d(a, {
+                dKL: function () {
+                    return r;
+                },
+                hX: function () {
+                    return l;
+                },
+            });
+            var h = n(8357);
+            function r(t) {
+                return (0, h.w_)({
+                    tag: "svg",
+                    attr: { viewBox: "0 0 24 24" },
+                    child: [
+                        {
+                            tag: "g",
+                            attr: {},
+                            child: [
+                                { tag: "path", attr: { fill: "none", d: "M0 0h24v24H0z" } },
+                                { tag: "path", attr: { fillRule: "nonzero", d: "M13.503 2.001l-10 10 3.083 3.083 13.08-13.083h-6.163zm-.006 9.198L8.122 16.62 13.494 22h6.189l-5.387-5.4 5.389-5.4h-6.188z" } },
+                            ],
+                        },
+                    ],
+                })(t);
+            }
+            function l(t) {
+                return (0, h.w_)({
+                    tag: "svg",
+                    attr: { viewBox: "0 0 24 24" },
+                    child: [
+                        {
+                            tag: "g",
+                            attr: {},
+                            child: [
+                                { tag: "path", attr: { fill: "none", d: "M0 0h24v24H0z" } },
+                                { tag: "path", attr: { d: "M1 3h4l7 12 7-12h4L12 22 1 3zm8.667 0L12 7l2.333-4h4.035L12 14 5.632 3h4.035z" } },
+                            ],
+                        },
+                    ],
+                })(t);
+            }
+        },
+    },
+]);
